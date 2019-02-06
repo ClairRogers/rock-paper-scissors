02a7f5fe5e5ce63b744448d57fef8ef4a487cc11
Clair Rogers <clair-rogers@outlook.com>
2019-02-06 15:00:50 -0700

    made the game

---
+++ app/app.js
@@ -0,0 +1,75 @@
+
+
+let rock = 'rock'
+let scissors = 'scissors'
+let paper = 'paper'
+
+let cChoices = {
+  rock: 'Rock',
+  paper: 'Paper',
+  scissors: 'Scissors'
+}
+
+
+function play(playersChoice) {
+  let compPlay = compChoice()
+  if (playersChoice == rock) {
+    if (compPlay == 'rock') {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="tied"><i class="far fa-dot-circle"></i></h1> It's a draw!`
+    } else if (compPlay == 'paper') {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="lost"><i class="far fa-times-circle"></i></h1> You lost!`
+    } else {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="won"><i class="far fa-check-circle"></i></h1> You won!`
+    }
+  } else if (playersChoice == paper) {
+    if (compPlay == 'rock') {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="won"><i class="far fa-check-circle"></i></h1> You win!`
+    } else if (compPlay == 'paper') {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="tied"><i class="far fa-dot-circle"></i></h1> It's a draw!`
+    } else {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="lost"><i class="far fa-times-circle"></i></h1> You lost!`
+    }
+  } else if (playersChoice == scissors) {
+    if (compPlay == 'rock') {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="lost"><i class="far fa-times-circle"></i></h1> You lost!`
+    } else if (compPlay == 'paper') {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="won"><i class="far fa-check-circle"></i></h1> You win!`
+    } else {
+      document.getElementById('result').innerHTML = `You played <b>${playersChoice}</b>, the computer played <b>${compPlay}</b>. <h1 class="tied"><i class="far fa-dot-circle"></i></h1> It's a draw!`
+    }
+  }
+}
+
+
+function compChoice() {
+  let choices = Object.keys(cChoices)
+  let choiceIndex = Math.floor(Math.random() * choices.length)
+  let compPlay = choices[choiceIndex]
+  return compPlay
+}
+
+
+
+
+
+
+
+
+
+
+//***** my own notes *********/
+
+
+// function play(playersChoice) {
+//   let compPlay = compChoice()
+//   document.getElementById('result').innerHTML = `You played ${playersChoice}, the computer played ${compPlay}`
+// }
+
+
+// function compChoice() {
+//   let choices = Object.keys(cChoices) //object.keys returns the initial properties of object
+//   let choiceIndex = Math.floor(Math.random() * choices.length) //mathFloor cuts off decimals. math Random gives a random number between 0 and the length of the choice arrary
+//   let compPlay = choices[choiceIndex]
+//   return compPlay
+// }
+
